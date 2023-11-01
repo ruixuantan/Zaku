18ab6a3eb4c4a3ec46f1d9941ba57d9d11209ebd
ruixuantan <ruixuantan@yahoo.com.sg>
2023-11-01 10:23:56 +0800

    Initialise integration test

---
+++ tests/test_query.rs
@@ -0,0 +1,18 @@
+use zaku::{
+    datatypes::record_batch::RecordBatch, error::ZakuError, logical_plans::dataframe::Dataframe,
+    sql::parser::parse,
+};
+
+fn execute(sql: &str) -> Result<RecordBatch, ZakuError> {
+    let path = "resources/test.csv".to_string();
+    let df = Dataframe::from_csv(&path)?;
+    let select_df = parse(sql, df)?;
+    let plan = select_df.logical_plan();
+    Ok(plan.to_physical_plan()?.execute())
+}
+
+#[test]
+fn basic_query() {
+    let sql = "SELECT * FROM test";
+    assert!(execute(sql).is_ok());
+}
